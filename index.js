ee80423006a16ab8c101a86310cb7a3f6b45e132
Bruno Prado <bruno.prado@tecnospeed.com.br>
2018-08-21 17:10:52 -0300

    start

---
+++ index.js
@@ -0,0 +1,43 @@
+const Mocha = require('mocha')
+const fs = require('fs')
+const Reporter = require('node-mocha-reporter')
+const SlackWebhook = require('slack-webhook')
+const path = require('path')
+let mocha = null
+const run = ({ dir, isDevelopment = false, timeout = 6000, urlSlack }, callback) => {
+  if (!fs.existsSync(dir)) return callback(new Error('Diretório de testes não existe.'))
+
+  if (!isDevelopment) {
+    mocha = new Mocha({ reporter: Reporter })
+  } else {
+    mocha = new Mocha({})
+  }
+
+  const files = fs.readdirSync(dir)
+  for (const file of files) {
+    console.log(file)
+    mocha.addFile(path.join(dir, file))
+  }
+
+  mocha.run(report => {
+    if (!isDevelopment) {
+      console.log(report)
+      const slack = new SlackWebhook(urlSlack, {
+        defaults: {
+          username: 'BotTest',
+          channel: '#sped_squad_log',
+          icon_emoji: ':robot_face:'
+        }
+      })
+      slack.send(JSON.stringify(report, null, 1)).then(a => console.log('aff'))
+    }
+  })
+}
+
+run({
+  dir: path.join(process.cwd(), 'tests'),
+  urlSlack: 'https://hooks.slack.com/services/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a'
+})
+module.exports = {
+  run
+}
---
+++ tests/index.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect
+describe('controllers/notas (OPERAÇÕES COM SUCESSO) ', () => {
+  it('POST /v1/evento/enviar - criar registro e inserir na fila', done => {
+    done()
+  })
+})
